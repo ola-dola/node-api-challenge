547936b50f8a6403148d87593db5a61d5f7790d6
Olamide Oredola <53713158+ola-dola@users.noreply.github.com>
2021-07-09 02:01:55 +0100

    Revert "Olamide oredola"

--- index.js
+++ index.js
@@ -12,11 +12,3 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
-
-const server = require('./server');
-
-const port = process.env.PORT || 5000;
-
-server.listen(port, () => {
-    console.log(`app listening on port ${port}`)
-});
